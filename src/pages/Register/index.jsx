7ba0dbb83e3e95235d8a70dbd622a1d6bb2e52fe
Thomas Raducha <thomasraducha@gmail.com>
2023-02-10 12:30:14 +0100

    create page Register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 // import Cookies from 'js-cookie';
+// import Usefetch from '../../hooks/Usefetch';
 import './index.scss';
 
 export default function Register() {
@@ -11,41 +13,53 @@ export default function Register() {
         password: ""
     });
 
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+
     //comportement
-    // const handleChange = (e) => {
-    //     setFormData({
-    //         ...formData,
-    //         [e.target.id]: e.target.value
-    //     });
-    // }
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    };
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const response = await axios.post('http://localhost:1337/auth/local/register', formData);
-    //         console.log(response);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError(null);
+        setIsLoading(true);
 
-    //         const jwt = response.data.jwt;
-    //         console.log("🚀 ~ file: index.jsx:30 ~ handleSubmit ~ jwt", jwt)
+        try {
+            const response = await axios.post('http://localhost:1337/auth/local/register', formData);
 
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
+            const jwt = response.data.jwt;
+            console.log("🚀 ~ file: index.jsx:30 ~ handleSubmit ~ jwt", jwt)
 
+            // store the JWT securely
+            // Cookies.set("jwt", jwt);
 
+            navigate("/");
+        } catch (error) {
+            console.error(error);
+            setError("An error occured while registering. Please try again.");
+        }
+        setIsLoading(false);
+    };
 
     //resultats
     return (
         <div className="form-container">
-            <form className="form" >{/* onSubmit={handleSubmit} */}
+            <form className="form" onSubmit={handleSubmit}>
+
+                {error && <div className="error">{error}</div>}
 
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
                     <input
                         type="text"
                         id="username"
-                    // onChange={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="form-group">
@@ -53,8 +67,7 @@ export default function Register() {
                     <input
                         type="email"
                         id="email"
-                    // onChange={handleChange}
-
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="form-group">
@@ -62,14 +75,13 @@ export default function Register() {
                     <input
                         type="password"
                         id="password"
-                    // onChange={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={isLoading}>
+                    {isLoading ? "Loading..." : "Submit"}
+                </button>
             </form>
         </div>
     );
 };
-
-
-
